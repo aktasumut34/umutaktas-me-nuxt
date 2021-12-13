3b046d718da1a4780dba87d17be5e67134b8ab92
Umut Aktaş <u.akts1@gmail.com>
2021-12-13 22:37:36 +0300

    remove unused button

--- components/TopBar.vue
+++ components/TopBar.vue
@@ -94,7 +94,7 @@
             />
           </svg>
         </div>
-        <div class="nav-text" @click="darkMode">
+        <div class="nav-text lg:pr-[5vw]" @click="darkMode">
           <svg
             v-show="currentMode == 'light'"
             xmlns="http://www.w3.org/2000/svg"
@@ -123,22 +123,6 @@
               stroke-linejoin="round"
               stroke-width="2"
               d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
-            />
-          </svg>
-        </div>
-        <div class="nav-text lg:pr-[5vw]">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="h-9 w-9"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4"
             />
           </svg>
         </div>
